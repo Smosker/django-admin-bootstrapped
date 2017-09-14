747a5850f3e5e9ad25315e92d956078f53b02c79
Vladimir Koljasinskij <smosker@yandex-team.ru>
2017-09-14 10:23:06 +0300

    change

--- django_admin_bootstrapped/static/admin/js/adlook.js
+++ django_admin_bootstrapped/static/admin/js/adlook.js
@@ -1,7 +1,7 @@
 /**
  * Created by smosker on 13.09.17.
  */
-$(document).ready(function() {
+/**$(document).ready(function() {
   $('.required').not( $('.inline-related').find('.required') ).append('*').attr("style","color: #FF0000 !important");
   $('.change-form > #container > #content > h1').after('<p style="color:#FF0000 !important"><br/>* denotes required fields.</p>');
 });
@@ -11,3 +11,4 @@ $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
 });
+*/
